e6c5ddc1c99d6766033ce377727cfb4a4de2b17d
Cake717 <124918871+Cake717@users.noreply.github.com>
2024-05-03 08:59:28 -0400

    created ceaser.py

---
+++ ceaser.py
@@ -0,0 +1,8 @@
+alphabet = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
+
+print()
+print('Ceaser Cypher')
+print()
+
+print('Encrypt of Decrypt? Please type the full word.')
+input = input('encrypt/decrypt: ').lower
